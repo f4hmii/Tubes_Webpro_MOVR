a6de6702b30d7bf404c44aed9ba60930402cfb00
f4hmii <fahmif3211@gmail.com>
2025-05-20 00:01:13 +0700

    hapus,tambah,produk

--- seller/hapus.php
+++ seller/hapus.php
@@ -14,8 +14,8 @@ if (isset($_GET['produk_id']) && is_numeric($_GET['produk_id'])) {
     $stmt2 = $conn->prepare("DELETE FROM produk WHERE produk_id = ?");
     $stmt2->bind_param("i", $id);
 
-    if ($stmt->execute()) {
-        header("Location: index.php");
+    if ($stmt2->execute()) {
+        header("Location: produk.php");
         exit();
     } else {
         echo "Gagal menghapus produk.";
