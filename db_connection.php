e1f6d10f28cddbaf17149b7e4f86e602ad4bc163
f4hmii <fahmif3211@gmail.com>
2025-06-04 09:16:34 +0700

    status pembayaran

--- db_connection.php
+++ db_connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "baruu";
+$dbname = "movr";
 
 // Buat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
